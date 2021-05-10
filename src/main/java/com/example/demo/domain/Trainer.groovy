01489a03693254aa6a55a0b70302be85f0e5dd11
JonathanRolon <rolon.jonathan.ezequiel@gmail.com>
2021-05-09 23:01:59 -0300

    agregada dependencia a java bean validator

--- src/main/java/com/example/demo/domain/Trainer.groovy
+++ src/main/java/com/example/demo/domain/Trainer.groovy
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.Table
 import javax.persistence.GenerationType
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
 
 @Entity
 @Table(name = "trainers")
@@ -15,9 +18,13 @@ class Trainer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id
 
+    @NotBlank(message = "Name is mandatory")
     @Column(nullable = false)
     String name
 
+    @Min(1)
+    @Max(5)
+    @NotBlank(message = "Level is mandatory")
     @Column(nullable = false)
     Short level
 }
